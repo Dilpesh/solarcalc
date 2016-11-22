74bfc9c0b828588ed9cb4cb95a081c5fd2dd9c42
Dilpesh <Dilpesh@users.noreply.github.com>
2016-11-22 11:17:51 +0530

    UI changed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 'use strict';
 var app = angular.module('SolarCalculatorApp', ['nvd3', 'rzModule', 'ui.bootstrap']);
 
-app.controller('solcarController', function ($scope) {
+app.controller('solcarController', function ($rootScope, $scope, $timeout) {
     $scope.error = '';
     $scope.loanyear = '';
     $scope.formSubmitted = false;
+    $scope.entryEmpty = true;
     var currBills = 0,
             reducedunits = 0,
             reducedbills = 0,
@@ -19,46 +20,106 @@ app.controller('solcarController', function ($scope) {
 
 
 
-
-    $scope.submit = function () {
-        $scope.solarsize = Math.min($scope.units / 120, $scope.roofsize / 100);
-        $scope.solarunits = $scope.solarsize * 4 * 30;
-        $scope.solarcost = 75000 * $scope.solarsize;
-        currBills = $scope.units * $scope.selectedCity.rates;
-        reducedunits = Math.max($scope.units - $scope.solarunits, 0);
-        reducedbills = reducedunits * $scope.selectedCity.rates;
-        $scope.formSubmitted = true;
-    };
-
-    $scope.checkMaxLoan = function () {
-        $scope.maxLoanAmt = (85 * $scope.solarcost) / 100;
-        if ($scope.loanamt > $scope.maxLoanAmt) //85% implement it - djain don't forget
+    $scope.calculator = function () {
+        if (!$scope.formSubmitted)
         {
-            $scope.error = 'Maximum Loan amount can be 85% of Solar SYstem cost';
+            return;
+        }
+        else if ($scope.selectedCity === '' || $scope.units === undefined || $scope.roofsize === undefined)
+        {
+            $scope.error = 'All fields are required.';
+            $scope.entryEmpty = true;
+
         }
         else
         {
             $scope.error = '';
-            $scope.loanyear = $scope.slider_floor_ceil.value;
-            interest = ($scope.loanamt * 11 * $scope.loanyear) / 100;
-            principal = $scope.loanamt;
-            totalmonths = $scope.loanyear * 12;
-            totalAmt = Number(principal) + Number(interest);
-            $scope.monemi = totalAmt / totalmonths;
-            $scope.monthlysavings = currBills - reducedbills - $scope.monemi;
-            $scope.data = feedDataInGraph();
+            $scope.entryEmpty = false;
+            $scope.solarsize = Math.min($scope.units / 120, $scope.roofsize / 100);
+            $scope.solarsize = (Math.round($scope.solarsize * 100)) / 100;
+            $scope.solarunits = Math.round($scope.solarsize * 4 * 30);
+            $scope.solarcost = 75000 * $scope.solarsize;
+            currBills = $scope.units * $scope.selectedCity.rates;
+            reducedunits = Math.max($scope.units - $scope.solarunits, 0);
+            reducedbills = reducedunits * $scope.selectedCity.rates;
+            $scope.formSubmitted = true;
+
+            $scope.sliderLoanAmt = {
+                value: $scope.solarcost / 2,
+                options: {
+                    floor: 0,
+                    ceil: (85 * $scope.solarcost) / 100,
+                    step: 100,
+                    showSelectionBar: true,
+                    onChange: function () {
+                        $scope.checkEmi();
+                    },
+                    onEnd: function () {
+                        $scope.checkEmi();
+                    }
+                }
+            };
+            $timeout(function () {
+                $scope.$broadcast('rzSliderForceRender');
+                $rootScope.$broadcast('rzSliderForceRender');
+                $scope.checkEmi();
+            });
+
         }
     };
 
-    //slider
-    $scope.slider_floor_ceil = {
+    $scope.submit = function () {
+        if ($scope.selectedCity === '' || $scope.units === undefined || $scope.roofsize === undefined)
+        {
+            $scope.error = 'All fields are required.';
+            $scope.formSubmitted = false;
+        }
+        else
+        {
+            $scope.error = '';
+            $scope.formSubmitted = true;
+            $scope.entryEmpty = false;
+            $scope.calculator();
+        }
+
+    };
+
+    $scope.checkEmi = function () {
+        if ($scope.formSubmitted)
+        {
+            $scope.maxLoanAmt = (85 * $scope.solarcost) / 100;
+            if ($scope.sliderLoanAmt.value > $scope.maxLoanAmt) //85% implement it - djain don't forget
+            {
+                $scope.error = 'Maximum Loan amount can be 85% of Solar SYstem cost';
+            }
+            else
+            {
+                $scope.error = '';
+                $scope.loanyear = $scope.sliderLoanTenure.value;
+                interest = ($scope.sliderLoanAmt.value * 11 * $scope.loanyear) / 100;
+                principal = $scope.sliderLoanAmt.value;
+                totalmonths = $scope.loanyear * 12;
+                totalAmt = principal + interest;
+                $scope.monemi = Math.round(totalAmt / totalmonths);
+                $scope.monthlysavings = Math.round(currBills - reducedbills - $scope.monemi);
+                $scope.data = feedDataInGraph();
+            }
+        }
+    };
+
+
+    $scope.sliderLoanTenure = {
         value: 6,
         options: {
             floor: 5,
             ceil: 7,
             step: 1,
+            showSelectionBar: true,
+            onChange: function () {
+                $scope.checkEmi();
+            },
             onEnd: function () {
-                $scope.checkMaxLoan();
+                $scope.checkEmi();
             }
         }
     };
@@ -107,7 +168,7 @@ app.controller('solcarController', function ($scope) {
                 axisLabelDistance: -100
             },
             callback: function (chart) {
-               // console.log("!!! lineChart callback !!!");
+                // console.log("!!! lineChart callback !!!");
             }
         },
         title: {
@@ -131,50 +192,58 @@ app.controller('solcarController', function ($scope) {
     function feedDataInGraph() {
         var noSolarArr = [], solarNoLoanArr = [], solarAndLoanArr = [];
 
-        var noSolar = 0,
-                solarNoLoan = Number($scope.solarcost),
-                solarAndLoan = Number($scope.solarcost) - Number($scope.loanamt);
-        //Data is represented as an array of {x,y} pairs.
-        for (var i = 0; i < 10; i++) {
-            noSolar = i * currBills * 12;
-            solarNoLoan = Number($scope.solarcost) + i * Number(reducedbills * 12);
-            if (i <= $scope.loanyear)
-            {
-                solarAndLoan = Number($scope.solarcost) - Number($scope.loanamt) + i * (Number($scope.monemi * 12) + Number(reducedbills * 12));
-            }
-            else
-            {
-                solarAndLoan += Number(reducedbills * 12);
-            }
-            noSolarArr.push({x: i, y: noSolar});
-            solarNoLoanArr.push({x: i, y: solarNoLoan});
-            solarAndLoanArr.push({x: i, y: solarAndLoan});
+        if ($scope.solarcost > 0)
+        {
+            var noSolar = 0,
+                    solarNoLoan = $scope.solarcost,
+                    solarAndLoan = $scope.solarcost - $scope.sliderLoanAmt.value;
+            //Data is represented as an array of {x,y} pairs.
+            for (var i = 0; i < 10; i++) {
+                noSolar = i * currBills * 12;
+                solarNoLoan = $scope.solarcost + i * reducedbills * 12;
+                if (i <= $scope.loanyear)
+                {
+                    solarAndLoan = $scope.solarcost - $scope.sliderLoanAmt.value + i * (($scope.monemi * 12) + reducedbills * 12);
+                }
+                else
+                {
+                    solarAndLoan += reducedbills * 12;
+                }
 
-        }
+                if (noSolar >= 0 && solarNoLoan > 0 && solarAndLoan > 0)
+                {
+                    noSolarArr.push({x: i, y: noSolar});
+                    solarNoLoanArr.push({x: i, y: solarNoLoan});
+                    solarAndLoanArr.push({x: i, y: solarAndLoan});
+                }
 
-        //Line chart data should be sent as an array of series objects.
-        return [
-            {
-                values: noSolarArr,
-                key: 'No Solar, just electricity bills',
-                color: '#ff7f0e'
+            }
+
+            //Line chart data should be sent as an array of series objects.
+            return [
+                {
+                    values: noSolarArr,
+                    key: 'No Solar, just electricity bills',
+                    color: '#ff7f0e'
 //                    key: 'Another sine wave',
 //                    color: '#7777ff',
 //                    area: true      //area - set to true if you want this line to turn into a filled area chart.
-            },
-            {
-                values: solarNoLoanArr,
-                key: 'Solar without loan',
-                color: '#0000FF'
-            },
-            {
-                values: solarAndLoanArr,
-                key: 'Solar with Oorjan Loan',
-                color: '#008000'
-            }
-        ];
+                },
+                {
+                    values: solarNoLoanArr,
+                    key: 'Solar without loan',
+                    color: '#0000FF'
+                },
+                {
+                    values: solarAndLoanArr,
+                    key: 'Solar with Oorjan Loan',
+                    color: '#008000'
+                }
+            ];
+        }
     }
     ;
+
 
 
 });
